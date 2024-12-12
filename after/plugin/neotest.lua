6dcb754f164b645ad2975376c847501f8db6db1a
Gali-Ketema Mbogo <gali_ketema_mbogo@homedepot.com>
2024-12-12 12:44:38 -0600

    add coverage

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -1,7 +1,7 @@
-vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>') 
-vim.keymap.set('n', '<leader>tf', ':TestFile<CR>') 
-vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>') 
-vim.keymap.set('n', '<leader>tl', ':TestLast<CR>') 
+vim.keymap.set('n', '<leader>tt', ':TestNearest COVERAGE=1<CR>') 
+vim.keymap.set('n', '<leader>tf', ':TestFile COVERAGE=1<CR>') 
+vim.keymap.set('n', '<leader>ts', ':TestSuite COVERAGE=1<CR>') 
+vim.keymap.set('n', '<leader>tl', ':TestLast COVERAGE=1<CR>') 
 
 vim.g["test#strategy"] = "basic"
 vim.g["test#basic#start_normal"] = 1

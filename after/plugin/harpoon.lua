2e9a3d26ff9a74962b3006cd698d566618d26e8a
Gali-Ketema Mbogo <gali_ketema_mbogo@homedepot.com>
2024-12-12 09:36:29 -0600

    change harpoon keybinds

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local harpoon = require("harpoon")
 harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 
-vim.keymap.set("n", "<C-i>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-o>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-m>", function() harpoon:list():next() end)
 
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)

74a49d35d60d87bbcfc4883932fae7e390640300
Gali-Ketema Mbogo <gali_ketema_mbogo@homedepot.com>
2025-01-23 11:20:36 -0600

    hcl

--- lua/kembo/plugins/treesitter.lua
+++ lua/kembo/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return{
     config = function ()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = {"bash", "go", "yaml", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = {"bash", "go", "yaml", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html","hcl" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },

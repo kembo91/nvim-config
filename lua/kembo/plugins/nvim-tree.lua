71d06ed50a640d1e0e264db7da6c8483ea100304
Gali-Ketema Mbogo <gali_ketema_mbogo@homedepot.com>
2025-01-28 14:50:32 -0600

    fix indenting

--- lua/kembo/plugins/nvim-tree.lua
+++ lua/kembo/plugins/nvim-tree.lua
@@ -10,15 +10,15 @@ return {
 	vim.g.loaded_netrwPlugin=1
 	vim.opt.termguicolors=true
 	require("nvim-tree").setup {
-			diagnostics={
-				enable=true,
-				show_on_dirs=true,
-				show_on_open_dirs=false,
-			},
-			update_focused_file={
-				enable=true,
-			}
+		diagnostics={
+			enable=true,
+			show_on_dirs=true,
+			show_on_open_dirs=false,
+		},
+		update_focused_file={
+			enable=true,
 		}
+	}
 	vim.keymap.set("n", "<C-q>", "<Cmd>NvimTreeToggle<CR>,{silent=true}")
   end,
 }

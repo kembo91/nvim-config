a9d06c1de43a2db184263f51e6c751b7e1bf4396
Gali-Ketema Mbogo <gali_ketema_mbogo@homedepot.com>
2025-01-28 14:26:30 -0600

    add snacks

--- lua/kembo/plugins/treesitter.lua
+++ lua/kembo/plugins/treesitter.lua
@@ -4,12 +4,13 @@ return{
     config = function ()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = {"bash", "go", "yaml", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html","hcl" },
+          ensure_installed = {"bash","terraform", "go", "yaml", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html","hcl" },
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },
-	vim.treesitter.language.register("bash", "curl")
+          indent = { enable = true }
         })
+	vim.treesitter.language.register("hcl", "tf")
+	vim.treesitter.language.register("bash", "curl")
 	return true
     end
 }

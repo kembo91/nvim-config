c041c416c76e496a9d529279f3bf02890cdc652e
Gali-Ketema <mbogo.g@promail366.com>
2025-01-28 20:29:45 -0600

    add fuzzy live grep

--- lua/kembo/plugins/telescope.lua
+++ lua/kembo/plugins/telescope.lua
@@ -1,11 +1,31 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = { 'nvim-lua/plenary.nvim', {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}},
 	config = function()
 		local telescope = require('telescope')
 		local builtin = require('telescope.builtin')
-		telescope.setup({})
+		telescope.setup({
+			extensions = {
+				fzf = {
+					fuzzy = true,                    -- false will only do exact matching
+					override_generic_sorter = true,  -- override the generic sorter
+					override_file_sorter = true,     -- override the file sorter
+					case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+				},
+			}
+		})
+		require('telescope').load_extension('fzf')
+		local function fuzzy_find_files()
+			builtin.grep_string({
+				path_display={'smart'},
+				only_sort_text=true,
+				word_match='-w',
+				search=''
+			})
+		end
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
+		vim.keymap.set('n', '<leader>ps', function ()
+			fuzzy_find_files()
+		end)
 	end
 }

fd2675a0eec3dfdad253e9b7b989dd7f2d9b2c93
kembo91 <ketema.galy@gmail.com>
2025-03-03 20:34:28 -0600

    treesitter

--- lua/kembo/init.lua
+++ lua/kembo/init.lua
@@ -2,6 +2,7 @@ vim.o.autowriteall = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.api.nvim_create_autocmd(
-  "FileType", {
-  pattern={"qf"},
-  command=[[nnoremap <buffer> <CR> <CR>:cclose<CR>]]})
+	"FileType", {
+		pattern = { "qf" },
+		command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
+	})
--- lua/kembo/plugins/treesitter.lua
+++ lua/kembo/plugins/treesitter.lua
@@ -7,7 +7,10 @@ return {
 			ensure_installed = { "bash", "terraform", "go", "yaml", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "hcl" },
 			sync_install = false,
 			highlight = { enable = true },
-			indent = { enable = true }
+			indent = { enable = true },
+			ignore_install = {},
+			auto_install = true,
+			modules = {}
 		})
 		vim.treesitter.language.register("hcl", "tf")
 		vim.treesitter.language.register("bash", "curl")

0d8843bdb5b36a771d3c5c51c5afc1bcdefc5193
kembo91 <ketema.galy@gmail.com>
2025-03-04 22:13:51 -0600

    add oil and remap

--- lua/kembo/remap.lua
+++ lua/kembo/remap.lua
@@ -1,14 +1,4 @@
 vim.g.mapleader = " "
-local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	desc = "autoformat go",
-	callback = function()
-		vim.cmd("GoImports")
-	end,
-	group = autocmd_group,
-})
-
 local ymalgrp = vim.api.nvim_create_augroup("yaml auto-commands", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*.yml",
@@ -29,4 +19,16 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = allgrp,
 })
 
+local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	desc = "autoformat go",
+	callback = function()
+		vim.cmd("GoImports")
+		vim.cmd("GoFmt")
+	end,
+	group = autocmd_group,
+})
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+

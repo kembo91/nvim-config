87960503889fa026669c6f0ebc5bf61a1791032a
kembo91 <ketema.galy@gmail.com>
2025-03-13 15:13:59 -0500

    c

--- lua/kembo/plugins/snacks.lua
+++ lua/kembo/plugins/snacks.lua
@@ -21,9 +21,6 @@ return {
 		end },
 	},
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
 		bigfile = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
@@ -40,6 +37,7 @@ return {
 			}
 
 		},
+		statuscolumn = { enabled = true },
 		dashboard = {
 			formats = {
 				key = function(item)

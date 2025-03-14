95991529f02c3f6de8b84eb8f46f5d69c661558a
kembo91 <ketema.galy@gmail.com>
2025-03-14 15:02:35 -0500

    changing things

--- lua/kembo/plugins/oil.lua
+++ lua/kembo/plugins/oil.lua
@@ -5,4 +5,11 @@ return {
 	opts = {},
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	lazy = false,
+	config = function()
+		require('oil').setup({
+			view_options = {
+				show_hidden = true
+			}
+		})
+	end
 }
